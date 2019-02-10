49fe32a36527ac961203768e0044c9c93a078491
root <root@localhost.localdomain>
2019-02-10 11:33:22 +0900

    CRLF->LF
    LOGFILE_PATH_CHANGE

--- mate.sh
+++ mate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-LOGFILE_DATEPART=`date "+%Y%m%d_%H%M%S"`"
-LOGFILE_NAME="mate_install.${LOGFILE_DATEPART}"
+LOGFILE_DATEPART=`date "+%Y%m%d_%H%M%S"`
+LOGFILE_NAME="mate_install.${LOGFILE_DATEPART}.log"
+LOGFILE_PATH="${HOME}/${LOGFILE_NAME}"
 chmod 700 mate_backend.sh
-./mate_backend.sh 2>&1 | tee "${LOGFILE_NAME}"
-echo  >> "${LOGFILE_NAME}"
-echo "RETURN CODE = ${?}" >> "${LOGFILE_NAME}"
+./mate_backend.sh 2>&1 | tee "${LOGFILE_PATH}"
+echo  >> "${LOGFILE_PATH}"
+echo "RETURN CODE = ${?}" >> "${LOGFILE_PATH}"
