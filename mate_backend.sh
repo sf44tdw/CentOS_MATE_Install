ada474b2a2ed511e2ae996d43caad26ba9968d6e
sf44tdw <sf44tdw@users.noreply.github.com>
2019-04-21 11:54:27 +0900

    多重起動防止

--- mate_backend.sh
+++ mate_backend.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #minimalインストール後に実施。
 echo "";echo `date`;echo "[BEGIN MATE INSTALL]"
+#多重起動防止機講
+
+# 同じ名前のプロセスが起動していたら起動しない。
+_lockfile="/tmp/`basename $0`.lock"
+ln -s /dummy $_lockfile 2> /dev/null || { echo 'Cannot run multiple instance.'; exit 9; }
+trap "rm $_lockfile; exit" 1 2 3 4 5 6 7 8 15
 
 echo "";echo `date`;echo "1[UPDATE]"
 yum -y update || exit 1
@@ -11,35 +17,35 @@ yum -y install yum-plugin-remove-with-leaves || exit 2
 echo "";echo `date`;echo "3[INSTALL EPEL]"
 yum -y install epel-release || exit 3
 
-echo "";echo `date`;echo "5[INSTALL MATE DESKTOP]"
-yum -y groupinstall "X Window system" || exit 5
-yum -y groupinstall "MATE Desktop" || exit 5
-systemctl set-default graphical.target || exit 5
+echo "";echo `date`;echo "4[INSTALL MATE DESKTOP]"
+yum -y groupinstall "X Window system" || exit 4
+yum -y groupinstall "MATE Desktop" || exit 4
+systemctl set-default graphical.target || exit 4
 
 #不要なソフトのアンインストール
-echo "";echo `date`;echo "6[UNINSTALL_DEFAULT_PLAYER]"
-yum -y remove --remove-leaves totem rhythmbox || exit 6
+echo "";echo `date`;echo "5[UNINSTALL_DEFAULT_PLAYER]"
+yum -y remove --remove-leaves totem rhythmbox || exit 5
 
 #サウンド出力用
 #サウンドカード制御とミキサー
-echo "";echo `date`;echo "7[INSTALL_SOUND_SUPPORT]"
-yum -y install alsa-lib alsa-firmware alsa-tools alsa-utils pulseaudio pavucontrol alsa-plugins-pulseaudio pulseaudio-utils|| exit 7
+echo "";echo `date`;echo "6[INSTALL_SOUND_SUPPORT]"
+yum -y install alsa-lib alsa-firmware alsa-tools alsa-utils pulseaudio pavucontrol alsa-plugins-pulseaudio pulseaudio-utils|| exit 6
 #あとはpavucontrol(ミキサー)で適当に設定する。
 #pulseaudio経由でないと面倒。ALSA直接だとfirefoxから音が出なかったりする。
 
-echo "";echo `date`;echo "8[CONFIGURE_INPUT_METHOD]"
+echo "";echo `date`;echo "7[CONFIGURE_INPUT_METHOD]"
 echo '
 export GTK_IM_MODULE=ibus
 export XMODIFIERS=@im=ibus
 export QT_IM_MODULE=ibus
-' > "/etc/profile.d/im_module.sh" || exit 8
+' > "/etc/profile.d/im_module.sh" || exit 7
 
 #システム ⇒ コントロールセンター ⇒ 自動起動するアプリ
 #設定項目 	設定値
 #名前 	ibus daemon
 #コマンド 	ibus-daemon -rdx
 #説明 	start ibus daemon
-echo "";echo `date`;echo "9[CONFIGURE_MATE_INPUT_METHOD]"
+echo "";echo `date`;echo "8[CONFIGURE_MATE_INPUT_METHOD]"
 echo '
 [Desktop Entry]
 Type=Application
@@ -50,6 +56,7 @@ Name[ja_JP]=ibus daemon
 Name=ibus daemon
 Comment[ja_JP]=start ibus daemon
 Comment=start ibus daemon
-' > "/etc/xdg/autostart/ibus-daemon.desktop" || exit 9
+' > "/etc/xdg/autostart/ibus-daemon.desktop" || exit 8
 
 echo "";echo `date`;echo "[COMPLETE MATE INSTALL]"
+rm $_lockfile
